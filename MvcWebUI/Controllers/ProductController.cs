5b2fa68eec5f6cbcfa4d904ab414724e2ffce852
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2020-03-18 09:30:27 +0300

    Product/Index added. Index shows product list

--- MvcWebUI/Controllers/ProductController.cs
+++ MvcWebUI/Controllers/ProductController.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Business.Abstract;
 using Microsoft.AspNetCore.Mvc;
+using MvcWebUI.Models;
 
 namespace MvcWebUI.Controllers
 {
     public class ProductController : Controller
     {
+        private IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
         public IActionResult Index()
         {
-            return View();
+            var model = new ProductListViewModel
+            {
+                Products = _productService.GetAll()
+            };
+            return View(model);
         }
     }
 }

ee60ee7f332415e688e5201d310661dbd10ae420
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2020-03-18 10:29:31 +0300

    Category View Component is added.

--- MvcWebUI/Controllers/ProductController.cs
+++ MvcWebUI/Controllers/ProductController.cs
@@ -16,11 +16,11 @@ namespace MvcWebUI.Controllers
         {
             _productService = productService;
         }
-        public IActionResult Index()
+        public IActionResult Index(int category)
         {
             var model = new ProductListViewModel
             {
-                Products = _productService.GetAll()
+                Products = category > 0 ? _productService.GetByCategory(category) : _productService.GetAll()
             };
             return View(model);
         }
